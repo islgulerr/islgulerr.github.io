2c8df18d0a3c6de261943db5f622b47d56627694
isilguler <isilguler8@gmail.com>
2021-05-23 22:05:35 +0300

    Update Guler_Activity8.js

--- Activity8/Guler_Activity8.js
+++ Activity8/Guler_Activity8.js
@@ -12,23 +12,23 @@ m_button.onclick = function(){
 	console.log('here',count);
 	if(count==3){
 		alert("Level 2")
-		timeout = timeout-100;
+		speed = speed-100;
 	}
 	if(count==6){
 		alert("Level 3")
-		timeout = timeout-100;
+		speed = speed-100;
 	}
 	if(count==9){
 		alert("Level 4")
-		timeout = timeout-100;
+		speed = speed-100;
 	}
 	if(count==12){
 		alert("Level 5")
-		timeout = timeout-100;
+		speed = speed-100;
 	}
 	if(count==15){
 		alert("Level 6")
-		timeout = timeout-100;
+		speed = speed-100;
 	}
 }
 
